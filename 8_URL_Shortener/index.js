658a50cd59866826796b1ec401deed47fda2a9aa
Dheeraj Kumar <dk8549644@gmail.com>
2026-01-20 23:09:43 +0530

    URL shortener project

---
+++ 8_URL_Shortener/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const connectToMongoDB  = require("./connectdb");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url")
+
+const app = express();
+const PORT = 8082;
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, { $push: {
+        visitHistory: {
+            timestamp: Date.now(),
+        }
+    }})
+
+    res.redirect(entry.redirectURL)
+})
+
+
+app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`));
